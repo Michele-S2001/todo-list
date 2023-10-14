f9968b2522e39b249ff888b3cdbb071d0b4a1943
MicheleSerafini <serafini.michele01@gmail.com>
2023-10-14 17:32:03 +0200

    Added: shake warning when the input is empty

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ const tasksFieldDomElement = document.querySelector('.card__body');
 // ascolto il click sull' add btn
 addDomElement.addEventListener('click', function () {
   // recupero il valore interno all'input
-  const inputValue = inputDomElement.value;
-  
+  let inputValue = inputDomElement.value;
+
   if (inputValue === "") {
-    inputDomElement.style.borderColor = "red";
+    inputDomElement.style.backgroundColor = "rgba(255, 0, 0, 0.3)";
+    inputDomElement.style.animation = "shaker 600ms linear";
   } else {
-    inputDomElement.style.borderColor = "black";
+    inputDomElement.style.backgroundColor = "transparent";
+    inputDomElement.style.animation = "";
     // creo l'elemento task
     const taskDomElement = document.createElement('div');
     taskDomElement.classList.add('task');
